391b7ebf188c824b87a09c7d612801c7f2ff5607
justinecj <justinejose008@gmail.com>
2025-01-12 19:13:26 +0530

    synchronus -node

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const cors = require('cors')
 
 const app = express()
-
-// cors use in server app
-app.use(cors({
-    origin : 'http://localhost:4200'
-}))
 
 app.use(express.json())
 
@@ -21,7 +15,6 @@ const jwtMiddleware = (req, res, next) => {
         // verify token
         const data = jwt.verify(token, 'supersecretkey12345')
         console.log(data);
-        req.currentAcno = data.currentAccount
         next()
     }
     catch{
@@ -34,46 +27,27 @@ const jwtMiddleware = (req, res, next) => {
 }
 
 app.post('/registor', (req,res) => {
-    // registor solving - Asynchronus
-    dataservice.registor(req.body.username, req.body.acno, req.body.password).then(result =>{
-        res.status(result.statusCode).json(result)
-    })
+    console.log(req)
+    const result = dataservice.registor(req.body.username, req.body.acno, req.body.password)
+
+    // if(result){
+    //     res.send("Registor Successfully")
+    // }
+    // else{
+    //     res.send("Already Registered")
+    // }
+
+    res.status(result.statusCode).json(result)
 })
 
 app.post('/login', (req,res) => {
-    // login solving Asynchronus
-    dataservice.login(req.body.acno, req.body.password).then(result =>{
-        res.status(result.statusCode).json(result)
-    })
+    const result = dataservice.login(req.body.acno, req.body.password)
+    res.status(result.statusCode).json(result)
 })
 
 app.post('/deposit', jwtMiddleware,(req,res) => {
-    // deposit solving Asynchronus
-    dataservice.deposit(req, req.body.acno, req.body.password, req.body.amt).then(result =>{
+    const result = dataservice.deposit(req.body.acno, req.body.password, req.body.amt)
     res.status(result.statusCode).json(result)
-    })
-})
-
-app.post('/withdraw', jwtMiddleware,(req,res) => {
-    // withdraw solving Asynchronus
-    dataservice.withdraw(req, req.body.acno, req.body.password, req.body.amt).then(result =>{
-    res.status(result.statusCode).json(result)
-    })
-})
-
-app.post('/transaction', jwtMiddleware,(req,res) => {
-    // withdraw solving Asynchronus
-    dataservice.getTransaction(req, req.body.acno).then(result =>{
-    res.status(result.statusCode).json(result)
-    })
-})
-
-// Delete Account API
-app.delete('/deleteAcc/:accno', jwtMiddleware,(req,res) => {
-    // delete solving Asynchronus
-    dataservice.deleteAcc(req, req.params.accno).then(result =>{
-    res.status(result.statusCode).json(result)
-    })
 })
 
 app.listen(3000, () => {
--- services/data.service.js
+++ services/data.service.js
@@ -1,247 +1,109 @@
 // import jsonwebtoken
 const jwt = require('jsonwebtoken')
 
-// import db.js
-const db = require('./db')
+// DataBase
+db = {
+    1000 : { "acno":1000, "username":"justine", "password":1000, "balance":5000 }
+}
 
 // Registor
 const registor = (username, acno, password) => {
-    // asynchronus
-    return db.Users.findOne({
-        acno
-    }).then(user => {
-        console.log(user);
-
-        if(user){
-            return{
-                status : false,
-                message: 'Already Registered. Please login',
-                statusCode: 401
-            }    
+    if(acno in db){
+        // return false
+        return{
+            status : false,
+            message: 'Already Registered. Please login',
+            statusCode: 401
         }
-        else {
-            // insert in db
-            const newuser = new db.Users({
-                acno,
-                username,
-                password,
-                balance : 0,
-                transaction:[]
-            })
+    }
 
-            newuser.save()
+    else {
+        db[acno] = {
+            acno,
+            username,
+            password,
+            "balance":0,
+            transaction:[]
 
-            return{
-                status : true,
-                message: "Registered Successfully",
-                statusCode: 200
-            }
-        }    
-    })
+        }
+        console.log(db)
+        // return true
+        return{
+            status : true,
+            message: "Registered Successfully",
+            statusCode: 200
+        }
+    }
 }
 
-const login = (acno, pswd) => {
-    // asynchronus
-    return db.Users.findOne({
-        acno,
-        password : pswd
-    }).then(user => {
-        if(user){
-            console.log(user);
-            currentuser = user.username
-            currentAccount = acno
-            // token generation
+const login = (acno, password) => {
+    if(acno in db){
+        if(password == db[acno]["password"]){
+            currentuser = db[acno]["username"]
+            currentaccount = acno
             token = jwt.sign({
                 // store account number inside token
                 currentAccount : acno
             }, 'supersecretkey12345')
-
             return{
                 status : true,
                 message: "Login Successfully",
                 statusCode: 200,
                 currentuser,
-                currentAccount,
+                currentaccount,
                 token
             }
         }
-
         else{
             return{
                 status : false,
-                message: 'Invalid Account Number or Password!!!',
+                message: "Incorrect Password",
                 statusCode: 401
-            }    
+            }
         }
 
-              
-    })
+    }
+    else{
+        return{
+            status : false,
+            message: "User does not exists",
+            statusCode: 401
+        }
+    }
 }
-        
-// deposit
-const deposit = (req, acno, password, amt) => {
-    var amount = parseInt(amt);
-    var currentAcno = req.currentAcno
-    return db.Users.findOne({
-        acno,
-        password
-    }).then(user => {
-        if(user){
-            if(acno != currentAcno){
-                return {
-                    status: false,
-                    message: "Permission Denied",
-                    statusCode: 401
-                }
-            }
 
-            user.balance += amount
-            user.transaction.push({
+const deposit = (acno, password, amt) => {
+    var amount = parseInt(amt);
+    if (acno in db) {
+        if (password == db[acno]['password']) {
+            db[acno]['balance'] += amount;
+            db[acno].transaction.push({
                 type: "CREDIT",
                 amount: amount
-            })
-            user.save()
+            });
+            console.log(db);
             return {
                 status: true,
-                message: amount + " deposited successfully.. New balance is " + user.balance,
+                message: amount + " deposited successfully.. New balance is " + db[acno]["balance"],
                 statusCode: 200
-            }
-        }
-        else{
+            };
+        } else {
             return {
                 status: false,
-                message: "Invalid Account no or Password",
+                message: "Incorrect Password",
                 statusCode: 401
-            }
+            };
         }
-    })
-}
-
-// withdraw
-const withdraw = (req, acno, password, amt) => {
-    var amount = parseInt(amt);
-    var currentAcno = req.currentAcno
-    return db.Users.findOne({
-        acno,
-        password
-    }).then(user => {
-        if(user){
-            if(acno != currentAcno){
-                return {
-                    status: false,
-                    message: "Permission Denied",
-                    statusCode: 401
-                }
-            }
-            if(user.balance > amount){
-                user.balance -= amount
-                user.transaction.push({
-                    type: "DEBIT",
-                    amount: amount
-                })
-            
-                user.save()
-                return {
-                    status: true,
-                    message: amount + " debited successfully.. New balance is " + user.balance,
-                    statusCode: 200
-                }
-            }
-            else{
-                return {
-                    status: false,
-                    message: "Insufficient balance!!!",
-                    statusCode: 401
-                }
-            }
-        }
-        else{
-            return {
-                status: false,
-                message: "Insufficient Account Number or Password!!!",
-                statusCode: 401
-            }
-        }
-    })
-}
-
-// Transaction
-const getTransaction = (req, acno) => {
-    var currentAcno = req.currentAcno
-    return db.Users.findOne({
-        acno
-    }).then(user => {
-        if(user){
-            if(acno != currentAcno){
-                return {
-                    status: false,
-                    message: "Permission Denied",
-                    statusCode: 401
-                }
-            }
-
-            return {
-                status: true,
-                statusCode: 200,
-                transaction: user.transaction
-            }
-        }
-        else{
-            return {
-                status: false,
-                message: "User does not exist!!!",
-                statusCode: 401
-            }
-        }
-    })
-}
-
-// Delete
-const deleteAcc = (req, acno) => {
-    var currentAcno = req.currentAcno
-    return db.Users.deleteOne({
-        acno
-    }).then(user => {
-        console.log(user)
-        if(! user){
-            return {
-                status: false,
-                message: "Operation filed!!!",
-                statusCode: 401
-            }
-        }
-        else{
-            if(acno != currentAcno){
-                return {
-                    status: false,
-                    message: "Permission Denied",
-                    statusCode: 401
-                }
-            }
-
-            if(user.deletedCount == 0){
-                return {
-                    status: false,
-                    message: "Already deleted!!!",
-                    statusCode: 401
-                }
-            }
-
-            return {
-                status: true,
-                statusCode: 200,
-                message: "Successfully deleted"
-            }
-        }
-    })
-}
+    } else {
+        return {
+            status: false,
+            message: "Account number not found",
+            statusCode: 404
+        };
+    }
+};
 
 
 module.exports = {
-    registor,
-    login,
-    deposit,
-    withdraw,
-    getTransaction,
-    deleteAcc
+    registor, login, deposit
 }
